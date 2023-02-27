57c3b6d89b6b3bc5fa6640dd493c31a3d7c39874
Nicola <nicolaandresr@gmail.com>
2023-02-27 15:17:55 -0400

    feat(node-server): Adding Videos entity routes and endpoints

---
+++ node-server/src/handlers/videos.ts
@@ -0,0 +1,44 @@
+import { Request } from 'express'
+import { handleValidationError } from '../lib/errorHandler'
+import { Video } from '../models/video'
+
+export const createVideo = async (req: Request) => {
+  await handleValidationError(async () => await Video.create(req.body))
+  return { message: 'Video created', status: 201 }
+}
+
+export const deleteVideo = async (req: Request) => {
+  const { id } = req.params
+  const videoToDelete = await Video.findByPk(id)
+
+  if (!videoToDelete) throw { message: 'Video not found', status: 404 }
+
+  await Video.destroy({ where: { id } })
+  return { message: 'Video deleted', status: 204 }
+}
+
+export const getAllVideos = async () => {
+  const videos = await Video.findAll()
+  return { message: 'Videos retrieved', data: videos, status: 200 }
+}
+
+export const updateVideo = async (req: Request) => {
+  const { id } = req.params
+  await handleValidationError(
+    async () => await Video.update(req.body, { where: { id } })
+  )
+
+  return { message: 'Video updated', status: 204 }
+}
+
+export const publishVideo = async (req: Request) => {
+  const { id } = req.params
+  const video = await Video.findByPk(id)
+
+  if (!video) throw { message: 'Video not found', status: 404 }
+
+  video.published = true
+  await video.save()
+
+  return { message: 'Video published', status: 204 }
+}
---
+++ node-server/src/routes/videos.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import {
+  createVideo,
+  getAllVideos,
+  updateVideo,
+  deleteVideo,
+  publishVideo,
+} from '../handlers/videos'
+import { middlewareErrorHandler } from '../lib/errorHandler'
+import { routeHandler } from '../lib/routeHandler'
+import { JWTMiddleware } from '../middleware/authentication'
+
+const videosRouter = express.Router()
+
+videosRouter.use(middlewareErrorHandler(JWTMiddleware))
+
+videosRouter.get('/', routeHandler(getAllVideos))
+videosRouter.post('/', routeHandler(createVideo))
+videosRouter.put('/:id', routeHandler(updateVideo))
+videosRouter.put('/:id/publish', routeHandler(publishVideo))
+videosRouter.delete('/:id', routeHandler(deleteVideo))
+
+export default videosRouter
